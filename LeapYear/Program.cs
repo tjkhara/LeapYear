4fffdba9541dd856463c172cb5b81bfb32452b6e
Tajeshwar Khara <tkhara@gmail.com>
2016-10-13 11:19:45 +0530

    Leap Year C Sharp
    Leap Year C Sharp first commit

---
+++ LeapYear/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeapYear
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            LeapYear();    
+        }
+
+        static void LeapYear()
+        {
+            int year = 0;
+            Console.WriteLine("Please enter a year");
+            year = Convert.ToInt32(Console.ReadLine());
+            
+            if(year%100 == 0 && year%400 == 0)
+            {
+                Console.WriteLine("The year is a leap year");
+
+            } else if (year%100 == 0 && year%400 != 0)
+            {
+                Console.WriteLine("Not a leap year");
+            } else if(year%4 == 0)
+            {
+                Console.WriteLine("The year you entered is a leap year.");
+            }
+            
+            else 
+
+            {
+                Console.WriteLine("The year you entered is not a leap year.");    
+            }
+        }
+    }
+}
